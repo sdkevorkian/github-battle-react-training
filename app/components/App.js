64647b3fd3160dd4e2f796e989aa2e9ce9f052ee
sdkevorkian <sdkevorkian@gmail.com>
2017-07-15 20:52:05 -0700

    halfway through router part

--- app/components/App.js
+++ app/components/App.js
@@ -1,15 +1,25 @@
 var React = require('react');
 var Popular = require('./Popular');
+var Nav = require('./Nav');
+var Home = require('./Home');
+var Battle = require('./Battle');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
 
 class App extends React.Component {
     render() {
         return (
-            <div className='container'>
-              <Popular />
+          <Router>
+           <div className='container'>
+           <Nav />
+              <Route exact path='/' component={Home} />
+              <Route path='/battle' component={Battle} />
+              <Route path='/popular' component={Popular} />
            </div>
+          </Router>
         )
     }
 }
-
 
 module.exports = App;
---
+++ app/components/Battle.js
@@ -0,0 +1,9 @@
+var React = require('react');
+
+class Test extends React.Component{
+  render(){
+    return (<h1>TEST</h1>);
+  }
+}
+
+module.exports = Test;

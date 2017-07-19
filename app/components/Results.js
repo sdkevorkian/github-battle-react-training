ae8a45db6f01e7bff813716936fc85d5cc7218df
sdkevorkian <sdkevorkian@gmail.com>
2017-07-18 18:39:14 -0700

    added input fields to battle

---
+++ app/components/Results.js
@@ -0,0 +1,13 @@
+var React=require('react');
+
+class Results extends React.Component{
+  render(){
+    return (
+        <div>
+          results
+        </div>
+    );
+  }
+}
+
+module.exports=Results;
